516b7342e7dd81170cf0c055048eccc4c062cb50
Bogdan Anton <contact@bogdananton.ro>
2014-10-22 00:56:32 +0300

    updates in example for phpunit.xml->backupStaticAttributes

--- examples/phpunit-configuration-1/tests/DB/getInstanceTest.php
+++ examples/phpunit-configuration-1/tests/DB/getInstanceTest.php
@@ -8,6 +8,16 @@ class DB_getInstance extends \PHPUnit_Framework_TestCase
 	 */
 	public function testGetinstanceWillReturnADbInstance()
 	{
-		$this->assertInstanceOf('\DB', DB::getInstance());
+		$this->assertInstanceOf('\DB', \DB::getInstance());
+	}
+
+	/**
+	 * default value for instance is null
+	 * if this test fails then you should add backupStaticAttributes = true in phpunit.xml
+	 */
+	public function testDefaultValueForInstanceIsNull()
+	{
+		$db = new \DB;
+		$this->assertNull(\PHPUnit_Framework_Assert::readAttribute($db, 'instance'));
 	}
 }
