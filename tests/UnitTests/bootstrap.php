a8cb07c2899c1e9f88d11faefa747d6f3e88096b
Serban Ghita <serbanghita@gmail.com>
2015-03-11 18:55:33 +0200

    Setup for tests from /tests/UnitTests/

---
+++ tests/UnitTests/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+$composer = dirname(__FILE__) . '/../../vendor/autoload.php';
+if (!file_exists($composer)) {
+    throw new \RuntimeException("Please run 'composer install' first to set up autoloading. $composer");
+}
+
+/**
+ * @var \Composer\Autoload\ClassLoader $autoloader
+ */
+$autoloader = include_once $composer;
