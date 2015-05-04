b437dffa0d7fe46e730f82f58a6ab5184224b7d3
Serban Ghita <serbanghita@gmail.com>
2015-05-04 20:50:57 +0300

    Demo before.

---
+++ tests/ApiClientMock/ApiClientTest.php
@@ -0,0 +1,15 @@
+<?php
+namespace UnitTests\ApiClientMock;
+
+class ApiClientTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * getPostById returns a complete request when the request was successful
+     */
+    public function testGetPostByIdReturnsACompleteRequestWhenTheRequestWasSuccessful()
+    {
+        $this->markTestIncomplete('implement me...');
+    }
+
+}
